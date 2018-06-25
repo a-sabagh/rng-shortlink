5507368cff0403652f1d100a1778f58528bf822d
asabagh <a.sabagh72@gmail.com>
2018-06-25 13:13:56 +0430

    make Abstraction of shortlink to mainlink Method

--- includes/class.controller.shortlink.php
+++ includes/class.controller.shortlink.php
@@ -45,16 +45,49 @@ class rngshl_controller {
     }
     
     public function add_shortlink_rewrite_rule(){
-        add_rewrite_rule("^p([0-9]+)/?$", 'index.php?p=$matches[1]',"top");
+        add_rewrite_rule("^p([0-9]+)/?$", 'index.php?shl_id=$matches[1]',"top");
         add_rewrite_tag("%shl_id%","([0-9]+)");
         flush_rewrite_rules();
     }
 
-    public function shortlink_to_mainlink(){
-        //(set/update) cookie
-        //click event set postmeta *shl_click_event*
-        //redirect to main link
+    private function get_cookie($cookie_name){}
 
+    private function set_cookie($cookie_name,$id){}
+
+    private function update_cookie($cookie_name,$id){}
+
+    private function remove_cookie($cookie_name){}
+
+    private function update_click_event($meta_key,$post_id){}
+
+    /*
+     * redirect shortlink to mainlink by post_id and shl_id query variable 
+     * 1.(set/update) cookie
+     * 2.click event set postmeta *shl_click_event
+     * 3.redirect to main link
+     */
+    
+    public function shortlink_to_mainlink(){
+        $id = get_query_var("shl_id");
+        if(!isset($id) || empty($id))
+            return;
+        $cookie_name = "shl_click_event";
+        $meta_key = "shl_click_event";
+        $permalink = get_the_permalink($id);
+        $cookie_value = $this->get_cookie($cookie_name);
+        if($cookie_value){
+            if(in_array($id, $cookie_value)){
+                wp_redirect($permalink);
+            }else{
+                $this->update_cookie($cookie_name,$id);
+                $this->update_click_event($meta_key,$id);
+                wp_redirect($permalink);
+            }
+        }else{
+            $this->set_cookie($cookie_name,$id);
+            $this->update_click_event($meta_key,$id);
+            wp_redirect($permalink);
+        }
     }
 
 }
