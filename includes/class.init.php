b5623ce7b07c80c8a66b4462dee8c263de3421de
asabagh <a.sabagh72@gmail.com>
2018-06-28 22:50:03 +0430

    adding admin style and script

--- includes/class.init.php
+++ includes/class.init.php
@@ -9,6 +9,7 @@ class rngshl_init {
         $this->version = $version;
         $this->slug = $slug;
         add_action('plugins_loaded', array($this, 'plugins_loaded'));
+        add_action("admin_enqueue_scripts",array($this,"admin_enqueue_scripts"));
         $this->load_modules();
     }
 
@@ -16,9 +17,17 @@ class rngshl_init {
         load_plugin_textdomain( $this->slug , false, RNGSHL_PRT . "/languages" );
     }
     
+    public function admin_enqueue_scripts($hook){
+        if($hook == "tools_page_shl_click_view"){
+            wp_enqueue_style("shl-click-view-style", RNGSHL_PDU . "admin/assets/css/style.css");
+            wp_enqueue_script("shl-click-view-scripts", RNGSHL_PDU . "admin/assets/js/script.js");
+        }
+    }
+    
     public function load_modules(){
         require_once 'class.controller.shortlink.php';
         require_once 'class.controller.settings.php';
+        require_once 'class.controller.clicked.php';
     }
 
 }
