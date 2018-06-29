e2b762230efe8da90b4bf6d4d017b801b7079458
asabagh <a.sabagh72@gmail.com>
2018-06-29 15:42:09 +0430

    fix pagination on load

--- includes/class.controller.clicked.php
+++ includes/class.controller.clicked.php
@@ -6,18 +6,24 @@ if (!defined('ABSPATH')) {
 
 class rngshl_click_view {
 
-    public $posts_per_page;
-    
-    public function __construct($number) {
-        $this->posts_per_page = $number;
+    private $posts_per_page;
+    private $paginate_count;
+
+    public function __construct($posts_per_page, $paginate_count) {
+        $this->posts_per_page = $posts_per_page;
+        $this->paginate_count = $paginate_count;
         add_action("admin_menu", array($this, "click_view_menu"));
     }
 
-    public function click_view_menu() {
-        add_submenu_page("tools.php", __("Click View Report", "rng-shortlink"), __("Click View"), "manage_options", "shl_click_view", array($this, "click_view_report"));
+    public function get_posts_per_page() {
+        return $this->posts_per_page;
     }
 
-    public function click_view_report() {
+    public function get_paginate_count() {
+        return $this->paginate_count;
+    }
+
+    public function posts_count_report() {
         $query_args = array(
             'meta_query' => array(
                 array(
@@ -25,13 +31,38 @@ class rngshl_click_view {
                     'type' => 'NUMERIC',
                     'value' => 0,
                     'compare' => '>'
-                ),
-            'posts_per_page' => $this->posts_per_page,
-            )
+                )
+            ),
+            'posts_per_page' => -1
+        );
+        return count(get_posts($query_args));
+    }
+
+    public function click_view_menu() {
+        add_submenu_page("tools.php", __("Click View Report", "rng-shortlink"), __("Click View"), "manage_options", "shl_click_view", array($this, "click_view_report"));
+    }
+
+    public function click_view_report() {
+        $current = 1;
+        $posts_per_page = $this->get_posts_per_page();
+        $paginate_count = $this->get_paginate_count();
+        $posts_count = $this->posts_count_report();
+
+        $query_args = array(
+            'meta_query' => array(
+                array(
+                    'key' => 'shl_click_event',
+                    'type' => 'NUMERIC',
+                    'value' => 0,
+                    'compare' => '>'
+                )
+            ),
+            'posts_per_page' => $this->posts_per_page
         );
         require_once RNGSHL_ADM . 'click-view/click-view-report.php';
+        require_once RNGSHL_ADM . 'click-view/click-view-pagination.php';
     }
 
 }
-
-new rngshl_click_view(15);
+//$paginate_count must be odd
+new rngshl_click_view(15,7);
