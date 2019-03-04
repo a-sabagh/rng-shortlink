f67f5b48c35b883423ffa4446c1703d30bc3566f
asabagh <a.sabagh72@gmail.com>
2019-03-04 15:08:22 +0330

    site_url change with home_url

--- admin/click-view/click-view-report-ajax.php
+++ admin/click-view/click-view-report-ajax.php
@@ -9,10 +9,8 @@ foreach ($posts as $post):
     <tr>
         <td><a href="<?php echo get_the_permalink($post->ID); ?>" target="_blank" title="<?php echo get_the_title($post->ID); ?>"><?php echo get_the_title($post->ID); ?></a></td>
         <td><?php echo get_the_date("F d,Y", $post->ID); ?></td>
-        <td><code><?php echo site_url() . "/p" . $post->ID; ?></code></td>
+        <td><code><?php echo home_url() . "/p" . $post->ID; ?></code></td>
         <td><?php echo get_post_meta($post->ID, "shl_click_event", TRUE); ?></td>
     </tr>
     <?php
 endforeach;
-
-
