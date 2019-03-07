f52456318fa64db2586d0b1d0aaee780f4f12d54
asabagh <a.sabagh72@gmail.com>
2019-03-07 13:30:32 +0330

    denied direct access 1551952810

--- public/shortcode-shortlink.php
+++ public/shortcode-shortlink.php
@@ -1,4 +1,8 @@
-
+<?php
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+?>
 <div class="align-left <?php echo esc_attr($atts['wrapper_class']); ?>">
     <br><code class="ltr align-left"><?php echo home_url() . "/p" . $post->ID; ?></code><br>
 </div> 
