25adbf1e5beded1c773975c524b793d51297ef04
asabagh <a.sabagh72@gmail.com>
2019-05-08 13:29:16 +0430

    debug 1557305945

--- rng-shortlink.php
+++ rng-shortlink.php
@@ -14,13 +14,13 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
 
-define(RNGSHL_FILE, __FILE__);
-define(RNGSHL_PRF, plugin_basename(__FILE__)); //rng-shortlink/rng-shortlink.php
-define(RNGSHL_PDU, plugin_dir_url(__FILE__));   //http://localhost:8888/rng-plugin/wp-content/plugins/rng-shortlink/
-define(RNGSHL_PRT, basename(__DIR__));          //rng-refresh.php
-define(RNGSHL_PDP, plugin_dir_path(__FILE__));  //Applications/MAMP/htdocs/rng-plugin/wp-content/plugins/rng-shortlink/
-define(RNGSHL_TMP, RNGSHL_PDP . "public/");     // view OR templates System for public 
-define(RNGSHL_ADM, RNGSHL_PDP . "admin/");      // view OR templates System for admin panel
+define("RNGSHL_FILE", __FILE__);
+define("RNGSHL_PRF", plugin_basename(__FILE__)); //rng-shortlink/rng-shortlink.php
+define("RNGSHL_PDU", plugin_dir_url(__FILE__));   //http://localhost:8888/rng-plugin/wp-content/plugins/rng-shortlink/
+define("RNGSHL_PRT", basename(__DIR__));          //rng-refresh.php
+define("RNGSHL_PDP", plugin_dir_path(__FILE__));  //Applications/MAMP/htdocs/rng-plugin/wp-content/plugins/rng-shortlink/
+define("RNGSHL_TMP", RNGSHL_PDP . "public/");     // view OR templates System for public 
+define("RNGSHL_ADM", RNGSHL_PDP . "admin/");      // view OR templates System for admin panel
 
 require_once 'includes/class.init.php';
 $refresh_init = new rngshl_init(0.5, 'rng-shortlink');
